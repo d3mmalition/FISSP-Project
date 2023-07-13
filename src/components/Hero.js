a072423e5d94a5fadd85af66fd34c60be40d5969
d3mmalition <ecarr.gardner@gmail.com>
2023-07-13 17:22:15 -0400

    init commit

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css'
+
+function Hero() {
+    return (
+        <div>
+            <h1>Welcome to FISSP!</h1>
+            {/* Add other content and components */}
+        </div>
+    );
+}
+
+export default Hero;
