35eba2edc62b6b1b42d39deb79f3242e34623c08
d3mmalition <ecarr.gardner@gmail.com>
2023-07-17 16:41:19 -0400

    create footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Footer.css';
+import { Email, Facebook, Instagram } from '@material-ui/icons';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="footer-left">
+                <div className="newsletter">
+                    <h3>Subscribe to our Newsletter</h3>
+                    <input type="email" placeholder="Enter your email" />
+                    <button>Subscribe</button>
+                </div>
+            </div>
+            <div className="footer-right">
+                <div className="social-icons">
+                    <a href="https://www.facebook.com">
+                        <Facebook fontSize="large" />
+                    </a>
+                    <a href="https://www.instagram.com">
+                        <Instagram fontSize="large" />
+                    </a>
+                    <a href="mailto:example@example.com">
+                        <Email fontSize="large" />
+                    </a>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
