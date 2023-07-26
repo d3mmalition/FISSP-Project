b6389d53aa9b1b5c1faf49fcd9195884ae41a05f
d3mmalition <ecarr.gardner@gmail.com>
2023-07-25 21:31:29 -0400

    removed newsletter

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,22 +5,22 @@ import { Email, Facebook, Instagram } from '@material-ui/icons';
 function Footer() {
     return (
         <footer className="footer">
-            <div className="footer-left">
+            {/* <div className="footer-left">
                 <div className="newsletter">
                     <h3>Subscribe to our Newsletter</h3>
                     <input type="email" placeholder="Enter your email" />
                     <button>Subscribe</button>
                 </div>
-            </div>
+            </div> */}
             <div className="footer-right">
                 <div className="social-icons">
-                    <a href="https://www.facebook.com">
+                    <a href="https://www.facebook.com/profile.php?id=100057315104982">
                         <Facebook fontSize="large" />
                     </a>
                     <a href="https://www.instagram.com">
                         <Instagram fontSize="large" />
                     </a>
-                    <a href="mailto:example@example.com">
+                    <a href="mailto:friendsofichetucknee@gmail.com">
                         <Email fontSize="large" />
                     </a>
                 </div>
