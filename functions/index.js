5ec01a9f4a981486cfcc131c453e4d221fb54e9f
d3mmalition <ecarr.gardner@gmail.com>
2023-08-14 16:35:17 -0400

    firebase

---
+++ functions/index.js
@@ -0,0 +1,32 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
+
+admin.initializeApp();
+
+const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'your-email@gmail.com',
+        pass: 'your-email-password'
+    }
+});
+
+exports.submitForm = functions.https.onRequest(async (req, res) => {
+    const { name, email, message } = req.body;
+
+    const mailOptions = {
+        from: 'your-email@gmail.com',
+        to: 'recipient@example.com',
+        subject: 'New Form Submission',
+        text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        res.status(200).send('Email sent successfully');
+    } catch (error) {
+        console.error('Error sending email:', error);
+        res.status(500).send('Error sending email');
+    }
+});
