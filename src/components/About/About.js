ce3b3320c10a43412959199c9bf069cd214e3afd
d3mmalition <ecarr.gardner@gmail.com>
2023-08-14 16:01:24 -0400

    added ethics

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,29 +5,88 @@ function About() {
 
     return (
         <div className="about-container">
-            <h2>About Ichetucknee State Park</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquam neque eu ligula elementum, sit amet malesuada odio posuere. Sed scelerisque tempus nisi, vitae posuere odio consectetur et.</p>
+            <h1>Friends of Ichetucknee Springs CSO Code of Ethics - February 2020</h1>
 
-            <h2>About Friends of Ichetucknee State Park (FISSP)</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vehicula massa quis tempor feugiat. Sed pharetra, justo et aliquet posuere, augue lacus auctor purus, a tempus ligula lorem vel nunc.</p>
+            <h2>FRIENDS OF ICHETUCKNEE SPRINGS STATE PARK<br /> Code of Ethics</h2>
 
-            <h2>Board Members</h2>
-            <div className="board-members">
-                <div className="member">
-                    <img src="member1.jpg" alt="Board Member 1" />
-                    <h3>John Doe</h3>
-                    <p>Position: President</p>
-                </div>
-                <div className="member">
-                    <img src="member2.jpg" alt="Board Member 2" />
-                    <h3>Jane Smith</h3>
-                    <p>Position: Vice President</p>
-                </div>
-                {/* Add more board members  */}
-            </div>
+            <h3>PREAMBLE</h3>
+            <p>
+                <ol>
+                    <li>It is essential to the proper conduct and operation of Friends of Ichetucknee Springs State Park
+                        (herein “CSO”) that its board members, officers, and employees be independent and impartial and
+                        that their position not be used for private gain. Therefore, the Florida Legislature in Section 112.3251,
+                        Florida Statute (Fla. Stat.), requiresthat the law protect against any conflict of interest and establish
+                        standards for the conduct of CSO board members, officers, and employees in situations where
+                        conflicts may exist.
+                    </li>
+                    <li> It is hereby declared to be the policy of the state that no CSO board member, officer, or employee
+                        shall have any interest, financial or otherwise, direct or indirect, or incur any obligation of any
+                        nature which is in substantial conflict with the proper discharge of his or her duties for the CSO.
+                        To implement this policy and strengthen the faith and confidence of the people in Citizen Support
+                        Organizations, there is enacted a code of ethics setting forth standards of conduct required of
+                        Friends of Ichetucknee Springs State Park board members, officers, and employees in the
+                        performance of their official duties.
+                    </li></ol></p>
+            <h3>STANDARDS</h3>
+            <p>The following standards of conduct are enumerated in Chapter 112, Fla. Stat., and are required by Section
+                112.3251, Fla. Stat., to be observed by CSO board members, officers, and employees.
+                <ol>
+                    <li>
+                        <b>
+                            Prohibition of Solicitation or Acceptance of Gifts</b><br />
+                        No CSO board member, officer, or employee shall solicit or accept anything of value to the recipient,
+                        including a gift, loan, reward, promise of future employment, favor, or service, based upon any
+                        understanding that the vote, official action, or judgment of the CSO board member, officer, or employee
+                        would be influenced thereby.
+                    </li><br />
+                    <li>
+                        <b>
+                            Prohibition of Accepting Compensation Given to Influence a Vote</b><br />
+                        No CSO board member, officer, or employee shall accept any compensation, payment, or thing of
+                        value when the person knows, or, with reasonable care, should know that it was given to influence a vote
+                        or other action in which the CSO board member, officer, or employee was expected to participate in his
+                        or her official capacity.
+                    </li>
+                    <br />
+                    <li><b>Salary and Expenses</b><br />
+                        No CSO board member or officer shall be prohibited from voting on a matter affecting his or her salary,
+                        expenses, or other compensation as a CSO board member or officer, as provided by law.
+                    </li><br />
 
-            <h2>Join Friends of Ichetucknee State Park (FISSP)</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquet felis id pharetra fringilla. Nulla scelerisque eros mi, at laoreet velit iaculis non. Vestibulum imperdiet enim ac nisi semper efficitur.</p>
+                    <li><b>Prohibition of Misuse of Position</b><br />
+                        A CSO board member, officer, or employee shall not corruptly use or attempt to use one’s official
+                        position or any property or resource which may be within one’s trust, or perform official duties, to secure
+                        a special privilege, benefit, or exemption.</li><br />
+                    <li><b>Prohibition of Misuse of Privileged Information</b><br />
+                        No CSO board member, officer, or employee shall disclose or use information not available to members
+                        of the general public and gained by reason of one’s official position for one’s own personal gain or
+                        benefit orforthe personal gain or benefit of any other person or business entity.
+                    </li><br />
+                    <li><b>Post-Office/Employment Restrictions</b><br />
+                        A person who has been elected to any CSO board or office or who is employed by a CSO may not
+                        personally represent another person or entity for compensation before the governing body of the CSO of
+                        which he or she was a board member, officer, or employee for a period of two years after he or she
+                        vacates that office or employment position.</li><br />
+                    <li><b>Prohibition of Employees Holding Office</b><br />
+                        No person may be, at one time, both a CSO employee and a CSO board member at the same time.
+                    </li><br />
+                    <li><b>Requirements to Abstain From Voting</b><br />
+                        A CSO board member or officer shall not vote in official capacity upon any measure which would affect
+                        his or her special private gain or loss, or which he or she knows would affect the special gain or any
+                        principal by whom the board member or officer is retained. When abstaining, the CSO board member
+                        or officer, prior to the vote being taken, shall make every reasonable effort to disclose the nature of his
+                        or her interest as a public record in a memorandum filed with the person responsible for recording the
+                        minutes of the meeting, who shall incorporate the memorandum in the minutes. If it is not possible for
+                        the CSO board member or officer to file a memorandum before the vote, the memorandum must be filed
+                        with the person responsible for recording the minutes of the meeting no later than 15 days after the vote.
+                    </li><br />
+                    <li><b>Failure to Observe CSO Code of Ethics</b><br />
+                        Failure of a CSO board member, officer, or employee to observe the Code of Ethics may result in the removal
+                        of that person from their position. Further, failure of the CSO to observe the Code of Ethics may result in the
+                        Florida Department of Environmental Protection terminating its Agreement with the CSO.
+                    </li><br />
+                    Draft July 15, 2014 | Code of Ethics accepted 2/10/2020
+                </ol></p>
         </div>
     );
 }
