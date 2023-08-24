3aa007a490c393cced2f7d507cf2e408665b8dc9
d3mmalition <ecarr.gardner@gmail.com>
2023-08-24 16:12:56 -0400

    membership tiers

--- src/components/Membership/Membership.js
+++ src/components/Membership/Membership.js
@@ -6,30 +6,42 @@ function Membership() {
         <div className="membership-container">
             <h1>Membership Tiers</h1>
             <div className="membership-tier">
-                <h2>Basic</h2>
+                <h2>Individual Membership</h2>
+                <h3>$15/year</h3>
+                <hr></hr>
+                <p><b>Individual</b> members get access to the following benefits:</p>
                 <ul>
-                    <li>Access to our online community</li>
-                    <li>Monthly newsletter</li>
+                    <li>12 Park Passes for one person</li>
+                    <li>Email Newsletter</li>
+                    <li>Volunteer Updates</li>
                 </ul>
-                <button>Join Basic</button>
+                <button>Join</button>
             </div>
             <div className="membership-tier">
-                <h2>Premium</h2>
+                <h2>Family - $25/year</h2>
                 <ul>
-                    <li>All Basic benefits</li>
-                    <li>Exclusive webinars and workshops</li>
-                    <li>Early access to new content</li>
+                    <li>All Individual benefits</li>
+                    <li>12 Park Passes for up to 8 family members</li>
                 </ul>
-                <button>Join Premium</button>
+                <button>Join "Family" Membership</button>
             </div>
             <div className="membership-tier">
-                <h2>Pro</h2>
+                <h2>Benefactor - $100/year</h2>
                 <ul>
-                    <li>All Premium benefits</li>
-                    <li>Personalized coaching sessions</li>
-                    <li>Access to premium resources</li>
+                    <li>All Family benefits</li>
+                    <li>?</li>
+                    <li>?</li>
                 </ul>
-                <button>Join Pro</button>
+                <button>Join "Benefactor Membership"</button>
+            </div>
+            <div className="membership-tier">
+                <h2>Lifetime Member - $500</h2>
+                <ul>
+                    <li>?</li>
+                    <li>?</li>
+                    <li>?</li>
+                </ul>
+                <button>Join "Lifetime Membership"</button>
             </div>
         </div>
     );
